56c08514fbfb1d81bb8f88d504a22d4fd611c239
Mike Skalandunas <mskalandunas@gmail.com>
2017-02-06 19:10:16 -0800

    start methods

--- riverine.js
+++ riverine.js
@@ -5,7 +5,66 @@ import ReactDOM from 'react-dom';
 import _u from './u';
 
 class Riverine extends React.Component {
+  constructor() {
+    super();
 
-};
+  }
+
+  componentDidMount() {
+
+  }
+
+  addHover(e) {
+
+  }
+
+  clickPercent(e) {
+
+  }
+
+  returnDuration() {
+
+  }
+
+  play() {
+
+  }
+
+  updateTime() {
+
+  }
+
+  handleHover() {
+
+  }
+
+  handlePlayhead() {
+
+  }
+
+  handleResize() {
+
+  }
+
+  mouseDown(e) {
+
+  }
+
+  mouseUp(e) {
+
+  }
+
+  movePlayhead(e) {
+
+  }
+
+  removeHover() {
+
+  }
+
+  render() {
+
+  }
+}
 
 export default Riverine;
