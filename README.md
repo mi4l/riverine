65336d12510daed5ea8001db7cbdb7962d9706d5
Mike Skalandunas <mskalandunas@gmail.com>
2017-02-07 18:51:33 -0800

    riverine added

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# riverine
-React audio wrapper.
+# Riverine
+
+A simple, lightweight wrapper for the HTML media element, specifically targeting audio functionality.  There is no styling included in this wrapper.
+
+    npm i riverine --save
+
+Supply a source file, a margin boolean, a hover boolean, a loop boolean, and CSS class names for each nested element.  If no class names or ID's are given, default classes/ID's are created.  The margin property is used to determine whether or not the playhead moves via margin change or via padding change.  If the margin is marked as false, it'll move by increasing padding.  Its default value is true.  Hover is used to add an option for mouseover progress selection.  This is set to true in the demo.
+
+Other available properties: playerId, playerClass, timelineClass, playheadClass, & durationClass.
+
+#### Sample usage:
+
+```
+import './css/main.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Riverine from 'riverine';
+
+const Root = () => (
+  <Riverine
+    source="audio/1.wav"
+  />
+);
+
+ReactDOM.render(<Root/>, document.querySelector('#root'));
+```
+
+Feel free to create an issue, or message me at [@mskalandunas](https://twitter.com/mskalandunas) if you have any questions.
--- riverine.js
+++ riverine.js
@@ -1,70 +1,217 @@
 'use strict';
 
-import React, { PropTypes } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import _u from './u';
+import _util_ from './u';
 
-class Riverine extends React.Component {
+class Riverine extends React.component {
   constructor() {
     super();
+    this.scrubberClicked = false;
+    this.duration        = '';
+    this.audioNode       = '';
+    this.playButton      = '';
+    this.playHead        = '';
+    this.timeline        = '';
+    this.timelineWidth   = '';
+    this.sourceDuration  = '';
 
+    this.addHover        = this.addHover.bind(this);
+    this.handleHover     = this.handleHover.bind(this);
+    this.handlePlayhead  = this.handlePlayhead.bind(this);
+    this.handleResize    = this.handleResize.bind(this);
+    this.mouseDown       = this.mouseDown.bind(this);
+    this.play            = this.play.bind(this);
+    this.removeHover     = this.removeHover.bind(this);
+    this.returnDuration  = this.returnDuration.bind(this);
+    this.updateTime      = this.updateTime.bind(this);
   }
 
   componentDidMount() {
+    const that         = ReactDOM.findDOMNode(this);
+    this.audioNode     = that.children[0];
+    this.duration      = that.children[1].children[1].children[2];
+    this.hover         = that.children[1].children[1].children[0];
+    this.playButton    = that.children[1].children[0];
+    this.playHead      = that.children[1].children[1].children[1];
+    this.timeline      = that.children[1].children[1];
+    this.timelineWidth = this.timeline.offsetWidth - this.playHead.offsetWidth;
 
+    this.audioNode.addEventListener('timeupdate', this.handlePlayhead.bind(this));
+    this.timeline.addEventListener('mouseover', this.handleHover.bind(this));
+    window.addEventListener('mouseup', this.mouseUp.bind(this));
+    window.addEventListener('resize', this.handleResize.bind(this));
   }
 
   addHover(e) {
+    let positionOffset = _u.handleOffsetParent(this.timeline);
+    let newMargLeft = e.pageX - positionOffset;
 
+    if (newMargLeft >= 0 && newMargLeft <= this.timelineWidth) {
+      this.hover.style.width = newMargLeft + 'px';
+    };
+
+    if (newMargLeft < 0) {
+      this.hover.style.width = '0px';
+    };
+
+    if (newMargLeft > this.timelineWidth) {
+      this.hover.style.width = this.timelineWidth + 'px';
+    };
   }
 
   clickPercent(e) {
-
+    let positionOffset = _u.handleOffsetParent(this.timeline);
+    return (e.pageX - positionOffset) / this.timelineWidth;
   }
 
   returnDuration() {
-
+    this.sourceDuration = this.audioNode.duration;
+    this.duration.innerHTML = _u.handleTime(this.audioNode.duration);
+    this.updateTime();
   }
 
   play() {
-
+    if (this.audioNode.paused) {
+      this.audioNode.play();
+      this.playButton.children[0].classList = '';
+      this.playButton.children[0].classList = 'fa fa-pause';
+    } else {
+      this.audioNode.pause();
+      this.playButton.children[0].classList = '';
+      this.playButton.children[0].classList = 'fa fa-play';
+    };
   }
 
   updateTime() {
+    this.duration.innerHTML = _u.handleTime(this.audioNode.currentTime) + ' / ' + _u.handleTime(this.audioNode.duration);
 
+    if (this.audioNode.currentTime === this.sourceDuration) {
+      this.playButton.children[0].classList = '';
+      this.playButton.children[0].classList = 'fa fa-play';
+    };
   }
 
   handleHover() {
-
+    if (this.props.hover) {
+      this.timeline.addEventListener('mousemove', this.addHover, false);
+      this.timeline.addEventListener('mouseout', this.removeHover, false);
+    };
   }
 
   handlePlayhead() {
+    let playPercent = this.timelineWidth * (this.audioNode.currentTime / this.audioNode.duration);
 
+    if (this.props.margin) {
+      this.playhead.style.marginLeft = playPercent + 'px';
+    } else {
+      this.playhead.style.paddingLeft = playPercent + 'px';
+    };
   }
 
   handleResize() {
+    let padding = this.playhead.style.paddingLeft;
+    let p;
 
+    padding === '' ? p = 0 : p = parseInt(padding.substring(0, padding.length - 2));
+    this.timelineWidth = (this.timeline.offsetWidth - this.playhead.offsetWidth) + p;
+    this.handlePlayhead();
   }
 
-  mouseDown(e) {
-
+  mouseDown() {
+    this.scrubberClicked = true;
+    window.addEventListener('mousemove', this.movePlayhead.bind(this));
+    this.audioNode.removeEventListener('timeupdate', this.handlePlayhead, false);
   }
 
   mouseUp(e) {
+    if (this.scrubberClicked === false) {
+      return;
+    };
 
+    this.movePlayhead(e);
+    window.removeEventListener('mousemove', this.movePlayhead.bind(this));
+    this.audioNode.currentTime = this.audioNode.duration * this.clickPercent(e);
+    this.audioNode.addEventListener('timeupdate', this.handlePlayhead, false);
+    this.scrubberClicked = false;
   }
 
   movePlayhead(e) {
+    let positionOffset = _u.handleOffsetParent(this.timeline);
+    let newMargLeft = e.pageX - positionOffset;
+    let n = this.playhead.style.width;
 
+    if (newMargLeft >= 0 && newMargLeft <= this.timelineWidth) {
+      n = newMargLeft + 'px';
+    };
+
+    if (newMargLeft < 0) {
+      n = '0px';
+    };
+
+    if (newMargLeft > this.timelineWidth) {
+      n = this.timelineWidth + 'px';
+    };
   }
 
   removeHover() {
-
+    this.hover.style.width = '0px';
   }
 
-  render() {
-
+  render : function() {
+    return (
+      <section>
+        <audio id={_util_.newId(this.props.playerId)} preload="true" onDurationChange={this.returnDuration} onTimeUpdate={this.updateTime} loop={this.props.loop}>
+          <source src={this.props.source}/>
+        </audio>
+        <div className={this.props.playerClass}>
+          <button className={this.props.playClass} onClick={this.play}></button>
+          <div className={this.props.timelineClass} onMouseDown={this.mouseDown}>
+            <div className={this.props.hoverClass}></div>
+            <div className={this.props.playheadClass} onMouseDown={this.mouseDown}></div>
+            <div className={this.props.durationClass}>
+              <span></span>
+            </div>
+            <div className={this.props.titleClass}>
+              <span>{this.props.title}</span>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
   }
-}
+};
+
+Riverine.defaultProps = {
+  durationClass : 'riverine-duration',
+  hover         : false,
+  hoverClass    : 'riverine-hover',
+  loop          : false,
+  margin        : false,
+  playClass     : 'riverine-play-button',
+  playheadClass : 'riverine-playhead',
+  playerClass   : 'riverine-player',
+  playerId      : 'riverine-',
+  pauseClass    : 'riverine-pause-button',
+  timelineClass : 'riverine-timeline',
+  titleClass    : 'riverine-title'
+};
+
+Riverine.propTypes = {
+  durationClass : React.PropTypes.string,
+  hover         : React.PropTypes.bool,
+  hoverClass    : React.PropTypes.string,
+  loop          : React.PropTypes.bool,
+  margin        : React.PropTypes.bool,
+  playClass     : React.PropTypes.string,
+  playheadClass : React.PropTypes.string,
+  playerClass   : React.PropTypes.string,
+  playerId      : React.PropTypes.string,
+  pauseClass    : React.PropTypes.string,
+  source        : React.PropTypes.string.isRequired,
+  timelineClass : React.PropTypes.string,
+  title         : React.PropTypes.string,
+  titleClass    : React.PropTypes.string
+};
 
 export default Riverine;
